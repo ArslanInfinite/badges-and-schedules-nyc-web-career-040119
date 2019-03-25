d76289cb06d4b0e6f7aa14de0d1348c0c5aaf4fb
Arslan Ali <ali.arslan319@gmail.com>
2019-03-25 22:23:35 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+# def batch_badge_creator(attendees)
+#   attendees.collect do |attendee|
+#     badge_maker(attendee)
+#   end
+# end
+
+
+def batch_badge_creator(attendees)
+  ada_lovelace = []
+  attendees.each do |attendee|
+     ada_lovelace << badge_maker(attendee)
+  end
+  return ada_lovelace
+end
