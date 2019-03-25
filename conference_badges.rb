f514790e1e2a1e296a487321eae50bdbe5464fc4
Arslan Ali <ali.arslan319@gmail.com>
2019-03-25 23:11:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,6 @@ end
 #   end
 # end
 
-
 def batch_badge_creator(attendees)
   new_attendees_array = []
   attendees.each do |attendee|
@@ -18,3 +17,33 @@ def batch_badge_creator(attendees)
   end
   return new_attendees_array
 end
+
+describe '#batch_badge_creator' do
+  it 'should return a list of badge messages' do
+    expect(batch_badge_creator(attendees)).to eq(badges)
+  end
+  it 'should not hard-code response' do
+    expect(batch_badge_creator(["Johnny"])).to eq(["Hello, my name is Johnny."])
+  end
+end
+
+def assign_rooms(attendees)
+  new_room_array = []
+attendees.each do |attendee|
+  new_room_array << badge_maker(attendee)
+  end 
+  return new_room_array
+end
+
+  describe '#assign_rooms' do
+
+    # Question 3
+
+    it 'should return a list of welcome messages and room assignments' do
+      expect(assign_rooms(attendees)).to eq(room_assignments)
+    end
+    it 'should not hard-code the response' do
+      expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
+    end
+
+  end  
