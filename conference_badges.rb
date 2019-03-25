661477211ea4481fb8ba67ca384727cd8599b568
Arslan Ali <ali.arslan319@gmail.com>
2019-03-25 23:17:25 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,12 +2,6 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-# def batch_badge_creator(attendees)
-#   attendees.collect do |attendee|
-#     badge_maker(attendee)
-#   end
-# end
-
 def batch_badge_creator(attendees)
   new_attendees_array = []
   attendees.each do |attendee|
