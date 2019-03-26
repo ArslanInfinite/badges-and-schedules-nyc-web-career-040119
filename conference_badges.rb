ccd3ec75e55051e76280cb01ada95f8c3b01440b
Arslan Ali <ali.arslan319@gmail.com>
2019-03-26 00:02:01 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,16 +11,16 @@ def batch_badge_creator(attendees)
 end
 
 # describe '#assign_rooms' do
-# 
+#
 #   # Question 3
-# 
+#
 #   it 'should return a list of welcome messages and room assignments' do
 #     expect(assign_rooms(attendees)).to eq(room_assignments)
 #   end
 #   it 'should not hard-code the response' do
 #     expect(assign_rooms(["Steve"])).to eq(["Hello, Steve! You'll be assigned to room 1!"])
 #   end
-# 
+#
 # end
 
 def assign_rooms(room_assignments)
